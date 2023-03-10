f2818a12b2bd293a74920760ec83cf7eca9b89e9
monika-czulak <czulak.monika@gmail.com>
2023-03-10 18:48:17 +0100

    Transactions JS

---
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -0,0 +1,38 @@
+import PropTypes from 'prop-types';
+
+const TransactionHistory = ({ items }) => {
+  return (
+    <table className="transaction-history">
+      <thead>
+        <tr>
+          <th>Type</th>
+          <th>Amount</th>
+          <th>Currency</th>
+        </tr>
+      </thead>
+
+      <tbody>
+      {items.map(item => (
+        <tr key={item.id}>
+          <td>{item.type}</td>
+          <td>{item.amount}</td>
+          <td>{item.currency}</td>
+        </tr>
+      ))}
+      </tbody>
+    </table>
+  );
+};
+
+TransactionHistory.propTypes = {
+    transactions: PropTypes.arrayOf(
+      PropTypes.shape({
+        id: PropTypes.string,
+        type: PropTypes.string,
+        amount: PropTypes.string,
+        currency: PropTypes.string
+      })
+    ),
+  };
+
+export default TransactionHistory
